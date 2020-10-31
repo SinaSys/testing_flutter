3e57088d33b27b514b820332be245ac34b2610d3
SinaSystem <sinasystem47@gmail.com>
2020-10-31 13:05:16 +0330

    commit

---
+++ test/counter_test.dart
@@ -0,0 +1,10 @@
+import 'package:testing_flutter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Counter value should be incremented', () {
+    final counter = Counter();
+    counter.increment();
+    expect(counter.value, 1);
+  });
+}
