386662fb96f34baf04384c281b0aba08dc749090
SinaSystem <sinasystem47@gmail.com>
2020-11-01 21:33:45 +0330

    commit third example

--- lib/calculate.dart
+++
@@ -1,5 +0,0 @@
-class Calculate{
-  String reverseString(String initial) {
-    return initial.split('').reversed.join();
-  }
-}
---
+++ lib/model/todo.dart
@@ -0,0 +1,18 @@
+class Todo {
+  String body;
+  bool finished;
+
+  Todo({
+    this.body,
+    this.finished,
+  });
+
+  @override
+  String toString() => 'Todo{body: $body, finished: $finished}';
+
+  @override
+  int get hashCode => body.hashCode + finished.hashCode;
+
+  @override
+  bool operator ==(other) => body == other.body && finished == other.finished;
+}
